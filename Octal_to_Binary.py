9121906239698058b5eae1c4795813312d74f284
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-10-12 22:20:32 +0530

    def bini(x):     temp=x     j=""     c=""     while(temp>0):         r=temp%2;         j+=str(r)         temp//=2     j=j[::-1]     k=len(j)     while(k<3):         c+="0"         k+=1     return c+j for i in range(int(input())):     x=input()     j=""     v=""     for i in range(1,len(x)):         j+=bini(int(x[i]))     temp=int(x[0])     while(temp>0):         r=temp%2;         v+=str(r)         temp//=2     print(v[::-1]+j)

---
+++ Octal_to_Binary.py
@@ -0,0 +1,27 @@
+def bini(x):
+    temp=x
+    j=""
+    c=""
+    while(temp>0):
+        r=temp%2;
+        j+=str(r)
+        temp//=2
+    j=j[::-1]
+    k=len(j)
+    while(k<3):
+        c+="0"
+        k+=1
+    return c+j
+for i in range(int(input())):
+    x=input()
+    j=""
+    v=""
+    for i in range(1,len(x)):
+        j+=bini(int(x[i]))
+    temp=int(x[0])
+    while(temp>0):
+        r=temp%2;
+        v+=str(r)
+        temp//=2
+    print(v[::-1]+j)
+    
